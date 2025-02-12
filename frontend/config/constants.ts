7525b27ebd53f79d083cfe2a29e1fe8e4b9b719f
imnahmed17 <asiknoman150@gmail.com>
2025-02-12 16:48:43 +0600

    suggestions have been implemented

--- frontend/config/constants.ts
+++ frontend/config/constants.ts
@@ -1,2 +1,2 @@
-export const LOGO_URL = process.env.NEXT_PUBLIC_LOGO_URL || 'https://skillgro-react.netlify.app/assets/img/logo/logo.svg';
-export const SECONDARY_LOGO_URL = process.env.NEXT_PUBLIC_SECONDARY_LOGO_URL || 'https://skillgro-react.netlify.app/assets/img/logo/secondary_logo.svg';
+export const LOGO_URL = process.env.NEXT_PUBLIC_LOGO_URL || '/assets/images/logo.png';
+export const SECONDARY_LOGO_URL = process.env.NEXT_PUBLIC_SECONDARY_LOGO_URL || '/assets/images/secondary-logo.png';
